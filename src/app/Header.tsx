c08fec9bc80694a97488ddcbcde01b3afe66b1e5
Oskar <o.skoglund09@gmail.com>
2023-05-03 23:03:04 +0200

    removed example assets

---
+++ src/app/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <main className="flex flex-col items-center py-4 bg-neutral-900">
+        <div>
+            <p className="text-6xl">DevJourney</p>
+            <p className="text-l">Tools, tricks, resources and inspiration</p>
+        </div>
+        </main>
+    )
+}
+
+export default Header
